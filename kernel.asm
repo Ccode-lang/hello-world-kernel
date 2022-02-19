e7b28161f5608a8d02551748c433444076854d33
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2022-02-19 15:00:53 -0700

    Update kernel.asm

--- kernel.asm
+++ kernel.asm
@@ -5,21 +5,5 @@ section .text
 global _start
 
 _start:
-    BITS 16
-    mov ah, 0x0e
-    
-    mov al, 'H'
-    int 0x10
-    mov al, 'e'
-    int 0x10
-    mov al, 'l'
-    int 0x10
-    mov al, 'l'
-    int 0x10
-    mov al, 'o'
-    int 0x10
-    
-    
-    
     ;forever loop
     jmp $
