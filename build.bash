7064b7e9cef74b3d94db0fb243dce3012436d33a
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2022-02-19 12:25:07 -0700

    Create build.bash

---
+++ build.bash
@@ -0,0 +1,10 @@
+git clone https://github.com/N00byEdge/bootelf
+cd bootelf
+git checkout 36692a211a24bd941d6178ba5460cbbd9974be0e
+nasm bootelf.asm -f bin -o bootsect.bin
+cd ..
+nasm -f elf kernel.asm
+ld kernel.o -o kernel.elf
+touch os.iso
+cat bootelf/bootsect.bin > os.iso
+cat kernel.elf > os.iso
