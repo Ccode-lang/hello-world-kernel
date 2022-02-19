72e7e6a30b9a54edc1067fb240be8d494976e312
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2022-02-19 13:05:23 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ cd bootelf
 git checkout 36692a211a24bd941d6178ba5460cbbd9974be0e
 nasm bootelf.asm -f bin -o bootsect.bin
 cd ..
-nasm -f elf kernel.asm
+nasm -f elf64 kernel.asm
 ld kernel.o -o kernel.elf
 touch os.iso
 cat bootelf/bootsect.bin > os.iso
