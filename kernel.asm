854db61b67d939cfb5cab4a268284d436c3a82e2
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2022-04-02 16:17:56 -0700

    Update kernel.asm

--- kernel.asm
+++ kernel.asm
@@ -8,7 +8,20 @@ section .text
         dd - (0x1BADB002 + 0x00) ;checksum. m+f+c should be zero
 
 global start
+global read_port
+global write_port
 extern kmain	        ;kmain is defined in the c file
+
+read_port:
+  mov edx, [esp + 4]
+  in al, dx
+  ret
+
+write_port:
+  mov   edx, [esp + 4]    
+  mov   al, [esp + 4 + 4]  
+  out   dx, al  
+  ret
 
 start:
   cli 			;block interrupts
