1f7136eaf69ed11ce955789657944b79c948b915
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2022-04-02 13:54:58 -0700

    make it print hello

--- kernel.asm
+++ kernel.asm
@@ -5,5 +5,18 @@ section .text
 global _start
 
 _start:
+    sti
+    mov ah, 0x0e
+    
+    mov al, ’H’
+    int 0x10
+    mov al, ’e’
+    int 0x10
+    mov al, ’l’
+    int 0x10
+    mov al, ’l’
+    int 0x10
+    mov al, ’o’
+    int 0x10
     ;forever loop
     jmp $
