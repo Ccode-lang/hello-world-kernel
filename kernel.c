61f61c98cb16fadde2d68ebea83fbf02796c0fee
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2022-04-02 14:51:12 -0700

    Create kernel.c

---
+++ kernel.c
@@ -0,0 +1,34 @@
+char *vidptr = (char*)0xb8000; 	//video mem begins here.
+unsigned int i = 0;
+unsigned int j = 0;
+
+
+void kmain(void) {
+  clear();
+  kprint("Hello world!");
+	return;
+}
+
+void clear() {
+  j = 0;
+  i = 0;
+	while(j < 80 * 25 * 2) {
+		/* blank character */
+		vidptr[j] = ' ';
+		/* attribute-byte - light grey on black screen */
+		vidptr[j+1] = 0x07; 		
+		j = j + 2;
+	}
+}
+
+void kprint(const char *str) {
+  j = 0;
+	while(str[j] != '\0') {
+		/* the character's ascii */
+		vidptr[i] = str[j];
+		/* attribute-byte: give character black bg and light grey fg */
+		vidptr[i+1] = 0x07;
+		++j;
+		i = i + 2;
+	}
+}
