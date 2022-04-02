fec8a75df06ad0e170fc9b1849f590aaa655ac71
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2022-04-02 14:36:53 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,9 @@
-git clone https://github.com/N00byEdge/bootelf
-cd bootelf
-git checkout 36692a211a24bd941d6178ba5460cbbd9974be0e
-nasm bootelf.asm -f bin -o bootsect.bin
-cd ..
-nasm -f elf64 kernel.asm
-ld kernel.o -o kernel.elf
-touch os.iso
-cat bootelf/bootsect.bin >> os.iso
-cat kernel.elf >> os.iso
+OUTPUT_FORMAT(elf32-i386)
+ENTRY(start)
+SECTIONS
+ {
+   . = 0x100000;
+   .text : { *(.text) }
+   .data : { *(.data) }
+   .bss  : { *(.bss)  }
+ }
