9928cdef517aaa0ebcd005502d53b51251153f0b
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2022-04-12 20:20:47 -0700

    yay!

--- kernel.c
+++ kernel.c
@@ -1,29 +1,35 @@
 #include "includes/colors.h"
 #include "includes/keymap.h"
 
+// true and false
 #define true 1
 #define false 0
 
+// import the commands from asm
 extern char read_port(unsigned short port);
 extern void write_port(unsigned short port, unsigned char data);
 
 char *vidptr = (char*)0xb8000; 	//video mem begins here.
+// location on screen in bytes
 unsigned int location = 0;
+// a counter used for everything
 unsigned int counter = 0;
+// keyboard status variable
 unsigned char kbstatus;
+// keyboard keycode variable
 char keycode;
-//right after video memory
+//right after video memory. used for storing the terminal command
 char *command = (char*)0xb8fa1;
+// a counter used for the keyboard
 unsigned int kbcounter = 0;
-char commandstr[80];
 
 void clear() {
 	counter = 0;
 	location = 0;
 	while(counter < 80 * 25 * 2) {
-		/* blank character */
+		// fill screen with blanks
 		vidptr[counter] = ' ';
-		/* attribute-byte - light grey on black screen */
+		// makes characters grey.
 		vidptr[counter+1] = 0x07; 		
 		counter = counter + 2;
 	}
@@ -31,10 +37,11 @@ void clear() {
 
 void kprint(const char *str, int color) {
 	counter = 0;
+	//run until end of string is found
 	while(str[counter] != '\0') {
-		/* the character's ascii */
+		// ascii of character written to the memory
 		vidptr[location] = str[counter];
-		/* attribute-byte: give character black bg and light grey fg */
+		// gives color to letter
 		vidptr[location+1] = color;
 		++counter;
 		location = location + 2;
@@ -48,30 +55,41 @@ void knewline() {
 }
 
 void term() {
-	while(1) {
+	while(true) {
+		// get keyboard status
 		kbstatus = read_port(0x64);
 		if (kbstatus & 0x01) {
+			//get current keycode
 			keycode = read_port(0x60);
 			if (keycode == 0x1C) {
+				//runs if enter is pressed
 				knewline();
 				counter = 0;
-				if (((char)command[0] == 't') && ((char)command[1] == 'e') && ((char)command[2] == 's') && ((char)command[3] == 't')) {
-					kprint("Test run.", VGA_COLOR_GREEN);
-					knewline();
+				
+				if (((char)command[0] == 'c') && ((char)command[1] == 'l') && ((char)command[2] == 'e') && ((char)command[3] == 'r')) {
+					clear();
+				} else if (((char)command[0] == 'e') && ((char)command[1] == 'x') && ((char)command[2] == 'i') && ((char)command[3] == 't')) {
+					return;
 				} else {
 					kprint("Invalid command.", VGA_COLOR_GREEN);
 					knewline();
 				}
+				
 				counter = 0;
+				// clear terminal input memory
 				while (counter != 200) {
 					command[counter] = ' ';
 					counter++;
 				}
+				// show new prompt
 				kbcounter = 0;
 				kprint("Terminal -$ ", VGA_COLOR_GREEN);
 			} else if (!(keycode < 0)) {
+				// print character on screen
 				vidptr[location++] = keyboard_map[(unsigned char) keycode];
 				vidptr[location++] = VGA_COLOR_GREEN;
+				
+				// add keyboard output to the input
 				command[kbcounter] = keyboard_map[(unsigned char) keycode];
 				kbcounter++;
 			} else {
@@ -85,13 +103,16 @@ void term() {
 }
 
 void kmain(void) {
+	// init keyboard
 	write_port(0x21 , 0xFD);
 	write_port(0x20, 0x20);
+	// get ready to load.
 	clear();
 	kprint("Hello, world!", VGA_COLOR_GREEN);
 	knewline();
 	kprint("Test", VGA_COLOR_GREEN);
 	knewline();
+	// start terminal
 	kprint("Terminal -$ ", VGA_COLOR_GREEN);
 	term();
 	return;
