271ca2b8ef274792dbdb5cefcfb7f35f3b4668b0
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2022-04-13 20:10:42 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 nasm -f elf32 kernel.asm -o kasm.o
-gcc -m32 -c kernel.c -o kc.o
-ld -m elf_i386 -fstack-protector -shared -T link.ld -o kernel.iso kasm.o kc.o
+gcc -fno-stack-protector -m32 -c kernel.c -o kc.o
+ld -m elf_i386 -T link.ld -o kernel.iso kasm.o kc.o
