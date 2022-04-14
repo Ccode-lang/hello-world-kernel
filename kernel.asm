fad229bb02b38f3f114bb5fd03208787daeef466
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2022-04-14 10:21:11 -0700

    add Idt (#3)
    * Update kernel.asm
    
    * Update kernel.c
    
    * Update kernel.asm
    
    * Update kernel.c
    
    * Update kernel.c
    
    * Update kernel.asm
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh
    
    * Update kernel.c
    
    * Update kernel.c
    
    * Update kernel.c
    
    * Update kernel.c

--- kernel.asm
+++ kernel.asm
@@ -10,8 +10,11 @@ section .text
 global start
 global read_port
 global write_port
+global load_idt
+global kbhandler
 
 extern kmain	        ;kmain is defined in the c file
+extern term
 
 
 ; Read and write borrowed from lisse (https://github.com/InsaneMiner/Lisse)
@@ -20,14 +23,20 @@ read_port:
 			;al is the lower 8 bits of eax
 	in al, dx	;dx is the lower 16 bits of edx
 	ret
-
+kbhandler:                 
+	call    term
+	iretd
 write_port:
 	mov   edx, [esp + 4]    
 	mov   al, [esp + 4 + 4]  
 	out   dx, al  
 	ret
 
-
+load_idt:
+	mov edx, [esp + 4]
+	lidt [edx]
+	sti
+	ret
 
 start:
   cli 			;block interrupts
--- kernel.c
+++ kernel.c
@@ -4,10 +4,15 @@
 // true and false
 #define true 1
 #define false 0
+#define IDT_SIZE 256
+#define INTERRUPT_GATE 0x8e
+#define KERNEL_CODE_SEGMENT_OFFSET 0x08
 
 // import the commands from asm
 extern char read_port(unsigned short port);
 extern void write_port(unsigned short port, unsigned char data);
+extern void load_idt(unsigned long *idt_ptr);
+extern void kbhandler(void);
 
 char *vidptr = (char*)0xb8000; 	//video mem begins here.
 // location on screen in bytes
@@ -22,6 +27,61 @@ char keycode;
 char *command = (char*)0xb8fa1;
 // a counter used for the keyboard
 unsigned int kbcounter = 0;
+
+
+
+
+struct IDT_entry {
+	unsigned short int offset_lowerbits;
+	unsigned short int selector;
+	unsigned char zero;
+	unsigned char type_attr;
+	unsigned short int offset_higherbits;
+};
+
+
+struct IDT_entry IDT[IDT_SIZE];
+
+
+
+void idt_init(void)
+{
+	unsigned long keyboard_address;
+	unsigned long idt_address;
+	unsigned long idt_ptr[2];
+
+	keyboard_address = (unsigned long)kbhandler;
+	IDT[0x21].offset_lowerbits = keyboard_address & 0xffff;
+	IDT[0x21].selector = KERNEL_CODE_SEGMENT_OFFSET;
+	IDT[0x21].zero = 0;
+	IDT[0x21].type_attr = INTERRUPT_GATE;
+	IDT[0x21].offset_higherbits = (keyboard_address & 0xffff0000) >> 16;
+
+	write_port(0x20 , 0x11);
+	write_port(0xA0 , 0x11);
+
+	write_port(0x21 , 0x20);
+	write_port(0xA1 , 0x28);
+
+	write_port(0x21 , 0x00);
+	write_port(0xA1 , 0x00);
+
+	write_port(0x21 , 0x01);
+	write_port(0xA1 , 0x01);
+
+
+	write_port(0x21 , 0xff);
+	write_port(0xA1 , 0xff);
+
+	idt_address = (unsigned long)IDT ;
+	idt_ptr[0] = (sizeof (struct IDT_entry) * IDT_SIZE) + ((idt_address & 0xffff) << 16);
+	idt_ptr[1] = idt_address >> 16 ;
+
+	load_idt(idt_ptr);
+}
+
+
+
 
 void clear() {
 	counter = 0;
@@ -54,59 +114,58 @@ void knewline() {
 	location = location + (line_size - location % (line_size));
 }
 
-void term() {
-	while(true) {
-		// get keyboard status
-		kbstatus = read_port(0x64);
-		if (kbstatus & 0x01) {
-			//get current keycode
-			keycode = read_port(0x60);
-			if (keycode == 0x1C) {
-				//runs if enter is pressed
-				knewline();
-				counter = 0;
-				
-				if (((char)command[0] == 'c') && ((char)command[1] == 'l') && ((char)command[2] == 'e') && ((char)command[3] == 'a') && ((char)command[4] == 'r')) {
-					clear();
-				} else if (((char)command[0] == 'e') && ((char)command[1] == 'x') && ((char)command[2] == 'i') && ((char)command[3] == 't')) {
-					kprint("Shutting down.", VGA_COLOR_GREEN);
-					return;
-				} else {
-					kprint("Invalid command.", VGA_COLOR_GREEN);
-					knewline();
-				}
-				
-				counter = 0;
-				// clear terminal input memory
-				while (counter != 200) {
-					command[counter] = ' ';
-					counter++;
-				}
-				// show new prompt
-				kbcounter = 0;
-				kprint("Terminal -$ ", VGA_COLOR_GREEN);
-			} else if (!(keycode < 0)) {
-				// print character on screen
-				vidptr[location++] = keyboard_map[(unsigned char) keycode];
-				vidptr[location++] = VGA_COLOR_GREEN;
-				
-				// add keyboard output to the input
-				command[kbcounter] = keyboard_map[(unsigned char) keycode];
-				kbcounter++;
+void term(void) {
+	write_port(0x20, 0x20);
+	// get keyboard status
+	kbstatus = read_port(0x64);
+	if (kbstatus & 0x01) {
+		//get current keycode
+		keycode = read_port(0x60);
+		if (keycode == 0x1C) {
+			//runs if enter is pressed
+			knewline();
+			counter = 0;
+			
+			if (((char)command[0] == 'c') && ((char)command[1] == 'l') && ((char)command[2] == 'e') && ((char)command[3] == 'a') && ((char)command[4] == 'r')) {
+				clear();
+			} else if (((char)command[0] == 'e') && ((char)command[1] == 'x') && ((char)command[2] == 'i') && ((char)command[3] == 't')) {
+				kprint("Shutting down.", VGA_COLOR_GREEN);
+				return;
 			} else {
-				;
+				kprint("Invalid command.", VGA_COLOR_GREEN);
+				knewline();
 			}
-		}
-		else {
+			
+			counter = 0;
+			// clear terminal input memory
+			while (counter != 200) {
+				command[counter] = ' ';
+				counter++;
+			}
+			// show new prompt
+			kbcounter = 0;
+			kprint("Terminal -$ ", VGA_COLOR_GREEN);
+		} else if (!(keycode < 0)) {
+			// print character on screen
+			vidptr[location++] = keyboard_map[(unsigned char) keycode];
+			vidptr[location++] = VGA_COLOR_GREEN;
+			
+			// add keyboard output to the input
+			command[kbcounter] = keyboard_map[(unsigned char) keycode];
+			kbcounter++;
+		} else {
 			;
 		}
+	}
+	else {
+		;
 	}
 }
 
 void kmain(void) {
+	idt_init();
 	// init keyboard
 	write_port(0x21 , 0xFD);
-	write_port(0x20, 0x20);
 	// get ready to load.
 	clear();
 	kprint("Hello, world!", VGA_COLOR_GREEN);
@@ -115,6 +174,5 @@ void kmain(void) {
 	knewline();
 	// start terminal
 	kprint("Terminal -$ ", VGA_COLOR_GREEN);
-	term();
 	return;
 }
