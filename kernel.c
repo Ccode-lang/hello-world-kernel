8ac30966f2c4df55623b55234be435c051e8d9d3
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2022-04-13 19:10:45 -0700

    Update kernel.c

--- kernel.c
+++ kernel.c
@@ -8,6 +8,7 @@
 // import the commands from asm
 extern char read_port(unsigned short port);
 extern void write_port(unsigned short port, unsigned char data);
+extern void load_idt(unsigned long *idt_ptr);
 
 char *vidptr = (char*)0xb8000; 	//video mem begins here.
 // location on screen in bytes
@@ -22,6 +23,61 @@ char keycode;
 char *command = (char*)0xb8fa1;
 // a counter used for the keyboard
 unsigned int kbcounter = 0;
+
+
+
+
+struct IDT_entry {
+	unsigned short int offset_lowerbits;
+	unsigned short int selector;
+	unsigned char zero;
+	unsigned char type_attr;
+	unsigned short int offset_higherbits;
+};
+
+
+struct IDT_entry IDT[IDT_SIZE];
+
+
+
+void idt_init(void)
+{
+	unsigned long keyboard_address;
+	unsigned long idt_address;
+	unsigned long idt_ptr[2];
+
+	keyboard_address = (unsigned long)keyboard_handler;
+	IDT[0x21].offset_lowerbits = keyboard_address & 0xffff;
+	IDT[0x21].selector = KERNEL_CODE_SEGMENT_OFFSET;
+	IDT[0x21].zero = 0;
+	IDT[0x21].type_attr = INTERRUPT_GATE;
+	IDT[0x21].offset_higherbits = (keyboard_address & 0xffff0000) >> 16;
+
+	write_port(0x20 , 0x11);
+	write_port(0xA0 , 0x11);
+
+	write_port(0x21 , 0x20);
+	write_port(0xA1 , 0x28);
+
+	write_port(0x21 , 0x00);
+	write_port(0xA1 , 0x00);
+
+	write_port(0x21 , 0x01);
+	write_port(0xA1 , 0x01);
+
+
+	write_port(0x21 , 0xff);
+	write_port(0xA1 , 0xff);
+
+	idt_address = (unsigned long)IDT ;
+	idt_ptr[0] = (sizeof (struct IDT_entry) * IDT_SIZE) + ((idt_address & 0xffff) << 16);
+	idt_ptr[1] = idt_address >> 16 ;
+
+	load_idt(idt_ptr);
+}
+
+
+
 
 void clear() {
 	counter = 0;
