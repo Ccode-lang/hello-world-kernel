37e885dd73641de659000d5a3c072c2d5c0eeb4a
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2022-04-15 16:01:17 -0700

    Update kernel.asm

--- kernel.asm
+++ kernel.asm
@@ -17,6 +17,41 @@ extern kmain	        ;kmain is defined in the c file
 extern term
 
 
+
+
+  gdt:
+ 
+  gdt_null:
+  dq 0
+ 
+  gdt_code:
+  dw 0FFFFh
+  dw 0
+ 
+  db 0
+  db 10011010b
+  db 11001111b
+  db 0
+ 
+  gdt_data:
+  dw 0FFFFh
+  dw 0
+ 
+  db 0
+  db 10010010b
+  db 11001111b
+  db 0
+ 
+  gdt_end:
+ 
+  gdt_desc:
+   dw gdt_end - gdt - 1
+   dd gdt
+
+
+
+
+
 ; Read and write borrowed from lisse (https://github.com/InsaneMiner/Lisse)
 read_port:
 	mov edx, [esp + 4]
@@ -40,6 +75,15 @@ load_idt:
 
 start:
   cli 			;block interrupts
+  lgdt [gdt_desc]
+  jmp 0x0008:fix_cs
+  fix_cs:
+  mov ax, 0x0010
+  mov ds, ax
+  mov es, ax
+  mov fs, ax
+  mov gs, ax
+  mov ss, ax
   mov esp, stack_space	;set stack pointer
   call kmain
   jmp $		 	;halt the CPU
